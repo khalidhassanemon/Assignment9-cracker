3e15bc9f4799a1763376cb49484d66070a1758fc
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-12-05 03:46:38 +0600

    adding-alert-for-correct-ans

--- assignment-cracker-app/src/Component/QuizOption/QuizOption.js
+++ assignment-cracker-app/src/Component/QuizOption/QuizOption.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const QuizOption = ({option}) => {
+const QuizOption = ({ option, correctAnswer }) => {
+    const handleClick=()=>{
+        if(option ===correctAnswer){
+            alert("Good Job!!This is correct Ans");
+    
+        }
+        else{
+            alert("Oh No..This is wrong ans");
+        }
+    }
     // console.log(option);
     return (
-        <div className='border-4 m-2'>
-            <button>
-             <ul className='list-disc list-inside'><li>{option}</li></ul></button>
-        </div>
+
+        <div className = 'border-4 m-2' >
+            <button onClick={handleClick}>
+                <ul className='list-disc list-inside'><li>{option}</li></ul></button>
+        </div >
     );
 };
 
